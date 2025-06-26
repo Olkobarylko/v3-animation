b1d1ce5418e388a5433f7be27049538518801bb4
Olkobarylko <olegbarylko81@gmail.com>
2025-06-26 15:00:15 +0300

    fixed animation

--- main.js
+++ main.js
@@ -1,287 +1,212 @@
 document.addEventListener("DOMContentLoaded", (event) => {
+  const infoItems = document.querySelectorAll(".wp-block-animation-v3__info-item");
+  const images = document.querySelectorAll(".wp-block-animation-v3__images-block");
+  const pinBlock = ".pin-spacer:has(.wp-block-animation-v3__pin-block)";
+
+  const infoItemShowDuration = 1000;
+  const infoItemHoldDuration = 2000;
+  const infoItemTotalDuration = infoItemShowDuration + infoItemHoldDuration;
+  const totalScrollHeight = infoItems.length * infoItemTotalDuration;
+
   ScrollTrigger.create({
     trigger: ".wp-block-animation-v3__pin-block",
     start: "top top",
-    end: "+=5000",
+    end: `+=${totalScrollHeight}`,
     pin: true,
     pinSpacing: true,
+    markers: false,
   });
 
   gsap.fromTo(
-  ".wp-block-animation-v3__time",
-  {
-    x: "50vw",
-    y: "0%",
-  },
-  {
-    x: "0",
-    y: "-50%",
-    top: "50%",
-    scrollTrigger: {
-      trigger: ".wp-block-animation-v3__wrapper",
-      start: "top+=200 bottom",
-      end: "center center",
-      scrub: 1,
+    ".wp-block-animation-v3__time",
+    {
+      x: "50vw",
+      y: "0%",
     },
-  }
-);
-
-//   gsap.to(".wp-block-animation-v3__time", {
-//     right: "50%",
-//     top: "50%",
-//     x: "50%",
-//     y: "-50%",
-//     scrollTrigger: {
-//       trigger: ".wp-block-animation-v3__wrapper",
-//       start: "top+=200 bottom",
-//       end: "center center",
-
-//       scrub: 1,
-//     },
-//   });
-});
-
-document.addEventListener("DOMContentLoaded", () => {
-  const items = document.querySelectorAll(".wp-block-animation-v3__info-item");
-  const blockDuration = 1000;
-
- items.forEach((item, index) => {
-  const triggerBlock = ".pin-spacer:has(.wp-block-animation-v3__pin-block)";
-  const subtitle = item.querySelector(".wp-block-animation-v3__info-subtitle");
-  const title = item.querySelector(".wp-block-animation-v3__info-title");
-  const text = item.querySelector(".wp-block-animation-v3__info-text");
-
-  const overlap = 500;
-  const baseStart = index * (2000 - overlap) ;
-  const baseEnd = baseStart + 2000;
-
-  gsap.to(item, {
-    scrollTrigger: {
-      trigger: triggerBlock,
-      start: `top+=${baseStart} top`,
-      end: `top+=${baseEnd} top`,
-      scrub: 1,
-    },
-    keyframes: [
-      { top: 500, opacity: 0 },
-      { top: 0, opacity: 1, duration: 1 },
-      { top: 0, opacity: 1, duration: 1 },
-      { top: -500, opacity: 0, duration: 0.5 },
-    ],
-  });
-
-  gsap.to(subtitle, {
-    scrollTrigger: {
-      trigger: triggerBlock,
-      start: `top+=${baseStart} top`,
-      end: `top+=${baseEnd} top`,
-      scrub: 1,
-    },
-    keyframes: [
-      { y: 0 },
-      { y: 0, duration: 1 },
-      { y: 0, duration: 1 },
-      { y: -400, duration: 0.5 },
-    ],
-  });
-
-  gsap.to(title, {
-    scrollTrigger: {
-      trigger: triggerBlock,
-      start: `top+=${baseStart} top`,
-      end: `top+=${baseEnd} top`,
-      scrub: 1,
-    },
-    keyframes: [
-      { y: 100 },
-      { y: 0, duration: 1 },
-      { y: 0, duration: 1 },
-      { y: -300, duration: 0.5 },
-    ],
-  });
-
-  gsap.to(text, {
-    scrollTrigger: {
-      trigger: triggerBlock,
-      start: `top+=${baseStart} top`,
-      end: `top+=${baseEnd} top`,
-      scrub: 1,
-    },
-    keyframes: [
-      { y: 200 },
-      { y: 0, duration: 1 },
-      { y: 0, duration: 1 },
-      { y: -300, duration: 0.5 },
-    ],
-  });
-});
-
- const images = document.querySelectorAll(
-    ".wp-block-animation-v3__images-block"
+    {
+      x: "0",
+      y: "-50%",
+      top: "50%",
+      scrollTrigger: {
+        trigger: ".wp-block-animation-v3__wrapper",
+        start: "top+=200 bottom",
+        end: "center center",
+        scrub: 1,
+      },
+    }
   );
 
-  let countPhaseStart = 0;
-  let countPhaseEnd = 0;
+  infoItems.forEach((item, index) => {
+    const subtitle = item.querySelector(".wp-block-animation-v3__info-subtitle");
+    const title = item.querySelector(".wp-block-animation-v3__info-title");
+    const text = item.querySelector(".wp-block-animation-v3__info-text");
+    const isLastItem = index === infoItems.length - 1;
 
-  images.forEach((item, index) => {
-    gsap.fromTo(
-      item,
-      { height: "0%", scale: 1 },
-      {
-        height: "100%",
-        scale: 1.5,
+    const itemStart = index * infoItemTotalDuration;
+    const itemEnd = itemStart + infoItemTotalDuration;
+
+    if (isLastItem) {
+      gsap.to(item, {
         scrollTrigger: {
-          trigger: ".pin-spacer:has(.wp-block-animation-v3__pin-block)",
-          start: `top+=${(index * 1500) + 1000} top`,
-          end: `top+=${((index + 1) * 1500) + 1000} top`,
+          trigger: pinBlock,
+          start: `top+=${itemStart} top`,
+          end: `top+=${itemEnd} top`,
+          scrub: 1,
+        },
+        keyframes: [
+          { top: 500, opacity: 0, duration: 0.5 },
+          { top: 0, opacity: 1, duration: 0.5 },
+          { top: 0, opacity: 1, duration: 2 },
+        ],
+      });
+    } else {
+      gsap.to(item, {
+        scrollTrigger: {
+          trigger: pinBlock,
+          start: `top+=${itemStart} top`,
+          end: `top+=${itemEnd} top`,
+          scrub: 1,
+        },
+        keyframes: [
+          { top: 500, opacity: 0, duration: 0.5 },
+          { top: 0, opacity: 1, duration: 0.5 },
+          { top: 0, opacity: 1, duration: 2 },
+          { top: -500, opacity: 0, duration: 0.5 },
+        ],
+      });
+    }
+
+    gsap.fromTo(
+      subtitle,
+      { y: 100, opacity: 0 },
+      {
+        y: 0,
+        opacity: 1,
+        duration: 0.6,
+        scrollTrigger: {
+          trigger: pinBlock,
+          start: `top+=${itemStart} top`,
+          end: `top+=${itemStart + infoItemShowDuration} top`,
+          scrub: 1,
+        },
+      }
+    );
+
+    gsap.fromTo(
+      title,
+      { y: 300, opacity: 0 },
+      {
+        y: 0,
+        opacity: 1,
+        duration: 0.8,
+        delay: 0.2,
+        scrollTrigger: {
+          trigger: pinBlock,
+          start: `top+=${itemStart} top`,
+          end: `top+=${itemStart + infoItemShowDuration} top`,
+          scrub: 1,
+        },
+      }
+    );
+
+    gsap.fromTo(
+      text,
+      { y: 500, opacity: 0 },
+      {
+        y: 0,
+        opacity: 1,
+        duration: 1,
+        delay: 0.4,
+        scrollTrigger: {
+          trigger: pinBlock,
+          start: `top+=${itemStart} top`,
+          end: `top+=${itemStart + infoItemShowDuration} top`,
           scrub: 1,
         },
       }
     );
   });
 
- 
+  images.forEach((image, index) => {
+    const imageStart = index * infoItemTotalDuration + infoItemShowDuration;
+    const imageEnd = imageStart + infoItemHoldDuration;
+
+    gsap.fromTo(
+      image,
+      { height: "0%", scale: 1 },
+      {
+        height: "100%",
+        scale: 1.5,
+        scrollTrigger: {
+          trigger: pinBlock,
+          start: `top+=${imageStart} top`,
+          end: `top+=${imageEnd} top`,
+          scrub: 1,
+        },
+      }
+    );
+  });
+
+  const timeBlock = document.querySelector(".time");
+  const timeValue = timeBlock.getAttribute("data-time");
+  const [targetHours, targetMinutes] = timeValue.split(":").map(Number);
+  
+  const columns = [
+    ".time-first-num",
+    ".time-second-num",
+    ".time-third-num",
+    ".time-fourth-num",
+  ];
+
+  function updateTimeDisplaySmooth(hours, minutes) {
+    const firstNum = Math.floor(hours / 10);
+    const secondNum = hours % 10;
+    const thirdNum = Math.floor(minutes / 10);
+    const fourthNum = minutes % 10;
+    
+    gsap.to(".time-first-num", {
+      y: `-${firstNum * 100}%`,
+      duration: 0.8,
+      ease: "sine.inOut"
+    });
+    
+    gsap.to(".time-second-num", {
+      y: `-${secondNum * 100}%`,
+      duration: 0.8,
+      ease: "sine.inOut",
+      delay: 0.1
+    });
+    
+    gsap.to(".time-third-num", {
+      y: `-${thirdNum * 100}%`,
+      duration: 0.8,
+      ease: "sine.inOut",
+      delay: 0.2
+    });
+    
+    gsap.to(".time-fourth-num", {
+      y: `-${fourthNum * 100}%`,
+      duration: 0.8,
+      ease: "sine.inOut",
+      delay: 0.3
+    });
+  }
+
+  updateTimeDisplaySmooth(0, 0);
+
+  ScrollTrigger.create({
+    trigger: ".wp-block-animation-v3__pin-block",
+    start: "top top",
+    end: `+=${totalScrollHeight}`,
+    onUpdate: (self) => {
+      const progress = self.progress;
+      const totalTargetMinutes = targetHours * 60 + targetMinutes;
+      const currentTotalMinutes = progress * totalTargetMinutes;
+      
+      const currentHours = Math.floor(currentTotalMinutes / 60);
+      const currentMinutes = Math.floor(currentTotalMinutes % 60);
+      
+      updateTimeDisplaySmooth(currentHours, currentMinutes);
+    }
+  });
 });
-
-
-document.addEventListener('DOMContentLoaded', () => {
-	const scrollSection = ".pin-spacer:has(.wp-block-animation-v3__pin-block)";
-
-	let timerActive = false;
-	const timeBlock = document.querySelector('.time');
-	const timeArea = document.querySelector('.time-area');
-	const timeValue = timeBlock.getAttribute('data-time');
-	let [targetMinutes, targetSeconds] = timeValue.split(':').map(Number);
-
-	let minutes = 0;
-	let seconds = 0;
-
-	const columns = [
-		'.time-first-num',
-		'.time-second-num',
-		'.time-third-num',
-		'.time-fourth-num',
-	];
-
-	let timerInterval;
-
-	function animateTime(min, sec) {
-		const timeDigits = [
-			Math.floor(min / 10),
-			min % 10,
-			Math.floor(sec / 10),
-			sec % 10,
-		];
-
-		columns.forEach((selector, index) => {
-			const column = document.querySelector(selector);
-			const digit = timeDigits[index];
-
-			if (!isNaN(digit)) {
-				gsap.to(column, {
-					y: `-${digit * 100}%`,
-					ease: 'power2.out',
-					duration: 1,
-				});
-			}
-		});
-	}
-	let blinkingTriggered = false;
-
-	function updateTime() {
-		if (minutes >= targetMinutes && seconds >= targetSeconds) {
-			clearInterval(timerInterval);
-			timeArea.classList.add('finished');
-			timeArea.classList.remove('blinking');
-			return;
-		}
-
-		if (
-			minutes === targetMinutes &&
-			seconds >= targetSeconds - 1 &&
-			!blinkingTriggered
-		) {
-			blinkingTriggered = true;
-			timeArea.classList.add('blinking');
-			setTimeout(() => {
-				timeArea.classList.remove('blinking');
-			}, 3000);
-		}
-
-		seconds++;
-		if (seconds === 60) {
-			seconds = 0;
-			minutes++;
-		}
-
-		animateTime(minutes, seconds);
-	}
-
-	function pauseTimer() {
-		clearInterval(timerInterval);
-	}
-
-	function resumeTimer() {
-		timerInterval = setInterval(updateTime, 1000);
-	}
-
-	function resetTimer() {
-		minutes = 0;
-		seconds = 0;
-		blinkingTriggered = false;
-		animateTime(minutes, seconds);
-		timeArea.classList.remove('finished');
-		timeArea.classList.remove('blinking');
-	}
-	let selfPgogress =  0;
-
-	setTimeout(() => {
-		ScrollTrigger.create({
-		trigger: scrollSection,
-		start: 'top center',
-		end: 'bottom center+=300',
-		once: false,
-		toggleActions: 'play none none none',
-		invalidateOnRefresh: true,
-		onUpdate: (self) => {
-			if (self.progress > 0 && !timerActive) {
-				resumeTimer();
-				timerActive = true;
-				timeArea.classList.remove('finished');
-			} else if (self.progress === 0 && timerActive) {
-				pauseTimer();
-				timerActive = false;
-				timeArea.classList.remove('finished');
-			}
-		},
-		onEnter: () => {
-			if (!timerActive) {
-				resumeTimer();
-				timerActive = true;
-			}
-			resetTimer();
-		},
-		onLeave: () => {
-			pauseTimer();
-			timerActive = false;
-			timeArea.classList.add('blinking');
-
-			setTimeout(() => {
-				timeArea.classList.add('finished');
-			}, 1000);
-		},
-		onEnterBack: () => {
-			resetTimer();
-		},
-		onLeaveBack: () => {
-			resetTimer();
-			timerActive = false;
-		},
-	});
-	}, 500);
-
-	pauseTimer();
-});
-
-
-
